81f536981f848646a75a4b608fb12e9f678d17ed
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2018-10-28 17:33:55 +0200

    Added todo-input component. Trying to make it work. Start in the wrong way. Need to display data at first.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
-
+    <todo-input></todo-input>
   </div>
 </template>
 
 <script>
+import TodoInput from './components/TodoInput.vue';
+
 export default {
   name: 'app',
+  components: {
+    TodoInput
+  },
   data () {
-
+    return {
+      tasks: [
+        { id: 1, name: 'Go to the park', done: true },
+        { id: 2, name: 'Make imposible', done: false },
+        { id: 3, name: 'Make additional bonus', done: true },
+        { id: 4, name: 'Make homework', done: false },
+        { id: 5, name: 'Go to bed', done: false },
+      ]
+    };
   }
 }
 </script>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text"
+           placeholder="Type your task.."
+           v-model="newValue">
+    <button @click="$emit('adding-new-task', newValue)">Add Todo</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        newValue: ''
+      };
+    },
+  }
+
+</script>
+
+<style>
+
+
+</style>
